7110324ecfd75a56a2c722305d9e6b8182e6cff7
Chayma <124793597+HeChayma@users.noreply.github.com>
2025-12-29 14:23:53 +0100

    Creation des dossier pour les micro-services

---
+++ Gateway_Service/src/main/java/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,25 @@
+package SecurityConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**", "/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+}
